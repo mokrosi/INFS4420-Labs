3e3cf1ecdc79c6dbd99f2e81043ed89d4f322493
krosi <krosidigital@gmail.com>
2025-10-09 14:09:46 +0400

    Add DB Conection and DB Oportion

---
+++ DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace INFS4420Labs
+{
+    public class DatabaseConnection
+    {
+        public string strConnString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Transportation.mdf;Integrated Security=True";
+        public string strSql;
+        public string strTableName;
+        public string strMsg;
+        public DataSet dstDataSet;
+        public DataTable tblMyTable;
+        public string strSelectedID;
+        public static string strStudentID = "";
+        public static string strReferenceNo = "";
+        public static string strCarPlateNo = "";
+        public static string strDriverID = "";
+        public static string strMoveFrom = "";
+        public static string strMoveTo = "";
+        public static string strDepTime = "";
+        public static string strReturnTime = "";
+    }
+}
---
+++ DatabaseOperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace INFS4420Labs
+{
+    public class DatabaseOperations
+    {
+        public DataSet PopulateDataset(string str_sql, string tblName)
+        {
+            //try
+            //{
+
+            // SqlConnection con; // Represents an open connection to a data source
+            // The SqlDataAdapter provides this bridge by using Fill 
+            // to load data from the data source into the DataSet
+            SqlConnection con;
+            SqlDataAdapter dad;
+            DatabaseConnection dbc = new DatabaseConnection();
+            con = new SqlConnection(dbc.strConnString);
+            dad = new SqlDataAdapter(str_sql, con);
+            dbc.dstDataSet = new DataSet();
+            dad.Fill(dbc.dstDataSet, tblName);
+            return dbc.dstDataSet;
+            //}
+
+
+
+            //catch (Exception ex)
+            //{
+            //    string script = "<script>alert('" + ex.Message + "');</script>";
+            //}
+        }
+        // This method handles database operation such as add, delete, or update
+        public void dbOperations(string src_sql, string msg)
+        {
+
+            DatabaseConnection dbc = new DatabaseConnection();
+            SqlConnection con = new SqlConnection(dbc.strConnString);
+            string str_Sql;
+            // SqlCommand - Represents an SQL statement 
+            // or stored procedure to execute against a data source.
+            SqlCommand cmd = new SqlCommand(src_sql, con);
+            con.Open();
+            str_Sql = src_sql;
+            // ExecuteNonQuery
+            // Executes a SQL statement against the Connection 
+            // and returns the number of rows affected.
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+    }
+}
