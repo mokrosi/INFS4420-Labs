26ecc5254ce28e49b2fc44583f99b609cccc46a5
Mohammed Zahran <s132814@student.squ.edu.om>
2025-10-12 10:38:44 +0400

    Add New page , my_transaction

---
+++ my_transaction.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="my_transaction.aspx.cs" Inherits="INFS4420Labs.my_transaction" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="grdMyTranscations" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+    </asp:GridView>
+</asp:Content>
---
+++ my_transaction.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace INFS4420Labs
+{
+    public partial class my_transaction : System.Web.UI.Page
+    {
+
+        DatabaseConnection dbc = new DatabaseConnection();
+        DatabaseOperations dbo = new DatabaseOperations();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dbc.strSql = "SELECT * FROM RequestedTrip";
+            dbc.strTableName = "RequestedTrip";
+            dbc.tblMyTable =dbo.PopulateDataset(dbc.strSql,dbc.strTableName).Tables[0];
+            grdMyTranscations.DataSource = dbc.tblMyTable;
+            grdMyTranscations.DataBind();
+
+
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
