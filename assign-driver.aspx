a79176a2439acca75187c3b071bb3374a8361dcc
mokrosi <KrosiDigital@gmail.com>
2025-10-21 11:57:40 +0400

    class 21-10 Done

---
+++ assign-driver.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteDriver.Master" AutoEventWireup="true" CodeBehind="assign-driver.aspx.cs" Inherits="INFS4420Labs.assign_driver" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table class="w-100">
+        <tr>
+            <td style="width: 349px">
+                Driver ID
+            </td>
+            <td style="width: 272px">
+                <asp:TextBox ID="txtDriverID" runat="server"></asp:TextBox>
+            </td>
+            <td rowspan="6">&nbsp;&nbsp;&nbsp;
+                <asp:Image ID="imgDriver" runat="server" Height="158px" style="margin-left: 29px" Width="152px" />
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 349px">
+                Driver Name</td>
+            <td style="width: 272px">
+                <asp:TextBox ID="txtDriverName" runat="server" CssClass="offset-sm-0"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td style="height: 21px; width: 349px">
+                Status</td>
+            <td style="height: 21px; width: 272px">
+                <asp:TextBox ID="txtStatus" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 349px">
+                <asp:Button ID="btnBack" runat="server" OnClick="btnBack_Click" Text="&lt;" Width="40px" />
+            </td>
+            <td style="width: 272px">
+                <asp:Button ID="btnNext" runat="server" OnClick="btnNext_Click" Text="&gt;" Width="40px" />
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 349px">
+                <asp:Button ID="btnBack2" runat="server" OnClick="btnBack2_Click" Text="&lt;&lt;" Width="40px" />
+            </td>
+            <td style="width: 272px">
+                <asp:Button ID="btnNext2" runat="server" OnClick="btnNext2_Click" Text="&gt;&gt;" Width="40px" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 34px">
+                <asp:Label ID="lblMasege" runat="server" ForeColor="Red" Text="lblMasege" Visible="False"></asp:Label>
+            </td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ assign-driver.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace INFS4420Labs
+{
+    public partial class assign_driver : System.Web.UI.Page
+    {
+
+        DatabaseConnection dbc = new DatabaseConnection();
+        DatabaseOperations dbo = new DatabaseOperations();
+        static int intRecordCounter;
+        static int intRowIndex = 0;
+        static DataTable tblDriver = new DataTable();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                dbc.strSql = "SELECT * FROM DRIVER";
+                dbc.strTableName = "DRIVER";
+                tblDriver = dbo.PopulateDataset(dbc.strSql, dbc.strTableName).Tables[0];
+                intRecordCounter = tblDriver.Rows.Count;
+                displayRecords(intRowIndex);
+            }
+
+        }
+
+        private void displayRecords(int intRowIndex)
+        {
+            txtDriverID.Text = tblDriver.Rows[intRowIndex][0].ToString();
+            txtDriverName.Text = tblDriver.Rows[intRowIndex][1].ToString();
+            txtStatus.Text = tblDriver.Rows[intRowIndex][2].ToString();
+            imgDriver.ImageUrl = "~/images/" + txtDriverID.Text + ".jpg";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            if (intRowIndex < intRecordCounter - 1)
+            {
+                lblMasege.Visible = false;
+                intRowIndex += 1;
+                displayRecords(intRowIndex);
+            }
+
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            if (intRowIndex > 0)
+            {
+                lblMasege.Visible = false;
+
+                intRowIndex -= 1;
+                displayRecords(intRowIndex);
+
+            }
+
+        }
+
+        protected void btnBack2_Click(object sender, EventArgs e)
+        {
+            intRowIndex = 0;
+            displayRecords(intRowIndex);
+
+            if (intRowIndex != 0)
+            {
+                lblMasege.Visible = false;
+                intRowIndex = 0;
+                displayRecords(intRowIndex);
+            }
+            else
+            {
+                lblMasege.Visible= true;
+                lblMasege.Text = "You are already at the First record.";
+            }
+        }
+
+        protected void btnNext2_Click(object sender, EventArgs e)
+        {
+            intRowIndex = intRecordCounter - 1;
+            displayRecords(intRowIndex);
+
+            if (intRowIndex != intRecordCounter - 1)
+            {
+                lblMasege.Visible = false;
+                intRowIndex = intRecordCounter - 1;
+                displayRecords(intRowIndex);
+            }
+            else
+            {
+                lblMasege.Visible = true;
+
+                lblMasege.Text = "You are already at the last record.";
+            }
+
+        }
+    }
+}
