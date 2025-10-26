eadd0459a687eed688a08de195f5ef6fa80c7a37
mokrosi <KrosiDigital@gmail.com>
2025-10-26 11:53:38 +0400

    class 26-10

---
+++ select-a-car.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteDriver.Master" AutoEventWireup="true" CodeBehind="select-a-car.aspx.cs" Inherits="INFS4420Labs.select_a_car" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table class="w-100">
+        <tr>
+            <td style="width: 371px; height: 49px">Brand</td>
+            <td style="height: 49px">
+                <asp:DropDownList ID="ddlcarList" runat="server" AutoPostBack="True" Height="39px" OnSelectedIndexChanged="ddlcarList_SelectedIndexChanged" Width="88px">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 371px">&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:GridView ID="grdCar" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Height="167px" Width="209px">
+                    <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                    <EditRowStyle BackColor="#999999" />
+                    <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                    <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                    <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                    <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                    <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                    <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                    <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                    <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+                </asp:GridView>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 371px">&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ select-a-car.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace INFS4420Labs
+{
+    public partial class select_a_car : System.Web.UI.Page
+    {
+
+        DatabaseConnection dbc = new DatabaseConnection();
+        DatabaseOperations dbo = new DatabaseOperations();
+        static DataTable tblCar = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int intRowcounter;
+            dbc.strSql = "SELECT * FROM CAR";
+            dbc.strTableName = "CAR";
+            tblCar = dbo.PopulateDataset(dbc.strSql, dbc.strTableName).Tables[0];
+            intRowcounter = tblCar.Rows.Count;
+            grdCar.DataSource = tblCar;
+            grdCar.DataBind();
+            int intCarIndex;
+            intCarIndex = 0;
+                
+                List<String> StrCarName = new List<String>();
+                while (intCarIndex < intRowcounter)
+                {
+                if (StrCarName.Contains(tblCar.Rows[intCarIndex][1].ToString()))
+                {
+                    intCarIndex++;
+                    continue;
+                }
+                else
+                {
+                    ddlcarList.Items.Add(tblCar.Rows[intCarIndex][1].ToString());
+                    StrCarName.Add(tblCar.Rows[intCarIndex][1].ToString());
+                    intCarIndex++;
+                }
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        protected void ddlCar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void ddlcarList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            grdCar.DataSource = null;
+            grdCar.DataBind();
+            int intRowcounter;
+            dbc.strSql = "SELECT * FROM CAR WHERE [Car_Name] = '" + ddlcarList.Text + "'";
+            dbc.strTableName = "CAR";
+            tblCar = dbo.PopulateDataset(dbc.strSql, dbc.strTableName).Tables[0];
+            intRowcounter = tblCar.Rows.Count;
+            grdCar.DataSource = tblCar;
+            grdCar.DataBind();
+
+        }
+    }
+}
